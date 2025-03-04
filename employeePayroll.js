0a713dc93cde75d72f83abd164caf9372c7b4038
Aryan Chaurasia <97691680+Aryanchaurasi@users.noreply.github.com>
2025-03-04 15:09:51 +0530

    Update employeePayroll.js

--- employeePayroll.js
+++ employeePayroll.js
@@ -13,8 +13,8 @@ class EmployeePayroll {
         `ID: ${this.id}, Name: ${this.name}, Salary: $${this.salary}, Gender: ${this.gender}, Start Date: ${this.startDate.toDateString()}`;
 }
 
-const emp1 = new EmployeePayroll(101, "HarshVeer", 150, "Trans", "2023-05-10");
-const emp2 = new EmployeePayroll(102, "Om Tiwari", 9000000, "Male", "2022-11-15");
+const emp1 = new EmployeePayroll(101, "Aryan chaurasia", 150, "Trans", "2023-05-10");
+const emp2 = new EmployeePayroll(102, "Ashu tiwari", 9000000, "Male", "2022-11-15");
 
 console.log(emp1.getDetails());
 console.log(emp2.getDetails());
